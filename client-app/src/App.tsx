f4d03b6ee9c6bfb332a101d68d71049e16e7fce5
Philip Krag Nielsen <phil5520@edu.campusvejle.dk>
2023-07-14 10:38:33 +0200

    Fetch activities from API with Axios.

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,9 +1,27 @@
+import { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [activities, setActivities] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/activities')
+      .then(response => {
+        setActivities(response.data);
+      });
+  }, []);
+
   return (
     <div className="App">
       <h1>Hello Reactivities!</h1>
+      <ul>
+        {activities.map((acitvity: any) => (
+          <li key={acitvity.id}>
+            {acitvity.title}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
