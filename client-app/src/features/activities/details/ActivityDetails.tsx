c65e2da0f8717f0b254e81f16e7b41eeda1bf346
Philip Krag Nielsen <phil5520@edu.campusvejle.dk>
2023-07-18 22:31:09 +0200

    Created UI form for creating & editing activity

--- client-app/src/features/activities/details/ActivityDetails.tsx
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -5,9 +5,10 @@ import { Activity } from "../../../app/models/activity";
 interface Props {
     activity: Activity;
     cancelSelectActivity: () => void;
+    openForm: (id: string) => void;
 }
 
-const ActivityDetails = ({activity, cancelSelectActivity}: Props) => {
+const ActivityDetails = ({activity, cancelSelectActivity, openForm}: Props) => {
     return (
         <Card fluid>
             <Image src={`/assets/categoryImages/${activity.category}.jpg`} />
@@ -22,7 +23,7 @@ const ActivityDetails = ({activity, cancelSelectActivity}: Props) => {
             </Card.Content>
             <Card.Content extra>
                 <Button.Group widths="2">
-                    <Button basis="blue" content="Edit" />
+                    <Button onClick={() => openForm(activity.id)} basis="blue" content="Edit" />
                     <Button onClick={cancelSelectActivity} basis="grey" content="Cancel" />
                 </Button.Group>
             </Card.Content>
