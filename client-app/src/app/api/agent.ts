5f8a6bcb24e93086b29945f313dca546e34082a2
Philip Krag Nielsen <phil5520@edu.campusvejle.dk>
2023-07-19 11:56:14 +0200

    Added loading indicator

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -1,7 +1,23 @@
 import axios, { AxiosResponse } from "axios";
 import { Activity } from "../models/activity";
+import { error } from "console";
+
+const sleep = (delay: number) => {
+    return new Promise((resolve) => {
+        setTimeout(resolve, delay)
+    });
+}
 
 axios.defaults.baseURL = "http://localhost:5000/api";
+axios.interceptors.response.use(async response => {
+    try {
+        await sleep(1000);
+        return response;
+    } catch (error) {
+        console.log(error);
+        return await Promise.reject(error);
+    }
+})
 
 const responseBody = <T> (response: AxiosResponse<T>) => response.data;
 
