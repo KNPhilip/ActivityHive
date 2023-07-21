31f38f6c25f945bb627c3c31263a47bfecb697f6
Philip Krag Nielsen <phil5520@edu.campusvejle.dk>
2023-07-21 12:34:15 +0200

    Created component for single activity-list item.

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,56 +1,17 @@
-import { SyntheticEvent, useState } from 'react';
-import { Segment, Item, Button, Label } from 'semantic-ui-react';
+import { Segment, Item } from 'semantic-ui-react';
 import { useStore } from '../../../app/stores/store';
 import { observer } from 'mobx-react-lite';
-import { Link } from 'react-router-dom';
+import ActivityListItem from './ActivityListItem';
 
 const ActivityList = () => {
-
     const { activityStore } = useStore();
-    const { deleteActivity, activitiesByDate: activities, loading } = activityStore;
-    const [target, setTarget] = useState('');
-
-    function handleActivityDelete(e: SyntheticEvent<HTMLButtonElement>, id: string) {
-        setTarget(e.currentTarget.name);
-        deleteActivity(id);
-    }
+    const { activitiesByDate: activities } = activityStore;
 
     return (
         <Segment>
             <Item.Group divided>
                 {activities.map(activity => (
-                    <Item key={activity.id}>
-                        <Item.Content>
-                            <Item.Header as="a">{activity.title}</Item.Header>
-                            <Item.Meta>{activity.date}</Item.Meta>
-                            <Item.Description>
-                                <div>
-                                    {activity.description}
-                                </div>
-                                <div>
-                                    {activity.city}, {activity.venue}
-                                </div>
-                            </Item.Description>
-                            <Item.Extra>
-                                <Button 
-                                    as={Link} 
-                                    to={`/activities/${activity.id}`} 
-                                    floated="right" 
-                                    content="View" 
-                                    color="blue"
-                                />
-                                <Button 
-                                    name={activity.id}
-                                    loading={loading && target === activity.id} 
-                                    onClick={(e) => handleActivityDelete(e, activity.id)} 
-                                    floated="right"
-                                    content="Delete" 
-                                    color="red"
-                                />
-                                <Label basic content={activity.category} />
-                            </Item.Extra>
-                        </Item.Content>
-                    </Item>
+                    <ActivityListItem key={activity.id} activity={activity} />
                 ))}
             </Item.Group>
         </Segment>
