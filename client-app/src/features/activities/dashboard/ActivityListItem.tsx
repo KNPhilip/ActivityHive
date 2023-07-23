40a28892ab28b51d8786be7375b865a768afb512
Philip Krag Nielsen <phil5520@edu.campusvejle.dk>
2023-07-23 18:39:43 +0200

    Home page styling

--- client-app/src/features/activities/dashboard/ActivityListItem.tsx
+++ client-app/src/features/activities/dashboard/ActivityListItem.tsx
@@ -1,8 +1,6 @@
 import { Link } from "react-router-dom";
 import { Button, Icon, Item, Segment } from "semantic-ui-react";
 import { Activity } from "../../../app/models/activity";
-import { useStore } from "../../../app/stores/store";
-import { SyntheticEvent, useState } from "react";
 import { observer } from "mobx-react-lite";
 
 interface Props {
@@ -10,15 +8,6 @@ interface Props {
 }
 
 const ActivityListItem = ({activity}: Props) => {
-    const { activityStore } = useStore();
-    const { deleteActivity, loading } = activityStore;
-    const [target, setTarget] = useState('');
-
-    function handleActivityDelete(e: SyntheticEvent<HTMLButtonElement>, id: string) {
-        setTarget(e.currentTarget.name);
-        deleteActivity(id);
-    }
-
     return (
         <Segment.Group>
             <Segment>
