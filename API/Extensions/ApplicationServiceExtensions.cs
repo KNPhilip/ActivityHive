2d844e6413e00deba792a27e7bf72d6254d9c666
Philip Krag Nielsen <phil5520@edu.campusvejle.dk>
2023-07-24 15:18:36 +0200

    Added simple FluentValidation rules

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -1,5 +1,7 @@
 using Application.Activities;
 using Application.Core;
+using FluentValidation;
+using FluentValidation.AspNetCore;
 using MediatR;
 
 namespace API.Extensions
@@ -20,6 +22,8 @@ namespace API.Extensions
             });
             services.AddMediatR(typeof(List.Handler));
             services.AddAutoMapper(typeof(MappingProfiles).Assembly);
+            services.AddFluentValidationAutoValidation();
+            services.AddValidatorsFromAssemblyContaining<Create>();
 
             return services;
         }
