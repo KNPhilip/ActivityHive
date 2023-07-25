1bdf88cf0a94cc422262d7fa6c3a146a59f45a92
Philip Krag Nielsen <phil5520@edu.campusvejle.dk>
2023-07-25 15:53:02 +0200

    Hooked up form submission to Formik.

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { Button, Segment } from 'semantic-ui-react';
+import { Button, Header, Segment } from 'semantic-ui-react';
 import { useStore } from '../../../app/stores/store';
 import { observer } from 'mobx-react-lite';
 import { Link, useNavigate, useParams } from 'react-router-dom';
@@ -34,17 +34,17 @@ const ActivityForm = () => {
     const validationSchema = Yup.object({
         title: Yup.string().required('The title is required.'),
         description: Yup.string().required('The description is required.'),
-        category: Yup.string().required(),
-        date: Yup.string().required(),
-        venue: Yup.string().required(),
-        city: Yup.string().required()
+        category: Yup.string().required('Please select a category.'),
+        date: Yup.string().required('The date is required.'),
+        venue: Yup.string().required('The venue field is required.'),
+        city: Yup.string().required('The city field is required.')
     });
 
     useEffect(() => {
         if (id) loadActivity(id).then(activity => setActivity(activity!))
     }, [id, loadActivity]);
 
-    function handleSubmit() {
+    function handleFormSubmit(activity: Activity) {
         if (!activity.id) {
             activity.id = uuid();
             createActivity(activity).then(() => navigate(`/activities/${activity.id}`));
@@ -56,13 +56,14 @@ const ActivityForm = () => {
 
     return (
         <Segment clearing>
+            <Header content="Activity Details" sub color="teal" />
             <Formik
                 validationSchema={validationSchema}
                 enableReinitialize 
                 initialValues={activity} 
-                onSubmit={values => console.log(values)}
+                onSubmit={values => handleFormSubmit(values)}
             >
-                {({ handleSubmit }) => (
+                {({ handleSubmit, isValid, isSubmitting, dirty }) => (
                     <Form className="ui form" onSubmit={handleSubmit} autoComplete="off" >
                         <MyTextInput placeholder="Title" name="title" />
                         <MyTextArea rows={3} placeholder="Description" name="description" />
@@ -74,10 +75,17 @@ const ActivityForm = () => {
                             timeCaption="Time"
                             dateFormat="MMMM d, yyyy h:mm aa"
                         />
+                        <Header content="Location Details" sub color="teal" />
                         <MyTextInput placeholder="City" name="city" />
                         <MyTextInput placeholder="Venue" name="venue" />
-                        <Button floated="right" positive type="submit" content="Submit" />
-                        <Button as={Link} to="/activities" disabled={loading} floated="right" type="button" content="Cancel" />
+                        <Button
+                            disabled={isSubmitting || !dirty || !isValid}
+                            loading={loading}
+                            floated="right" 
+                            positive type="submit" 
+                            content="Submit" 
+                        />
+                        <Button as={Link} to="/activities" floated="right" type="button" content="Cancel" />
                     </Form>
                 )}
             </Formik>
