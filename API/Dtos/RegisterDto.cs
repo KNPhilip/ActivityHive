bb01cedb26bc5e7c7e1379d2138a8bd68b6f2ef8
Philip Krag Nielsen <phil5520@edu.campusvejle.dk>
2023-07-26 11:53:05 +0200

    Created user dtos

---
+++ API/Dtos/RegisterDto.cs
@@ -0,0 +1,10 @@
+namespace API.Dtos
+{
+    public class RegisterDto
+    {
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string DisplayName { get; set; } = string.Empty;
+        public string Username { get; set; } = string.Empty;
+    }
+}
