7e404da2f9af56851a7112f41fc3ae4b85393880
Philip Krag Nielsen <phil5520@edu.campusvejle.dk>
2023-07-29 18:49:36 +0200

    Configured Activity/User relation & made migration.

---
+++ Domain/ActivityAttendee.cs
@@ -0,0 +1,11 @@
+namespace Domain
+{
+    public class ActivityAttendee
+    {
+        public string UserId { get; set; }
+        public User User { get; set; }
+        public Guid ActivityId { get; set; }
+        public Activity Activity { get; set; }
+        public bool IsHost { get; set; }
+    }
+}
