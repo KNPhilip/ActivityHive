6c87696f02c7d48ee38011622a0041c6d73023d9
Philip Krag Nielsen <phil5520@edu.campusvejle.dk>
2023-07-31 10:51:15 +0200

    Added attendance handler & endpoint.

--- Domain/Activity.cs
+++ Domain/Activity.cs
@@ -10,6 +10,6 @@ namespace Domain
         public string? City { get; set; }
         public string? Venue { get; set; }
         public bool IsCancelled { get; set; }
-        public ICollection<ActivityAttendee>? Attendees { get; set; }
+        public ICollection<ActivityAttendee> Attendees { get; set; } = new List<ActivityAttendee>();
     }
 }
