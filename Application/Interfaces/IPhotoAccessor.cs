72e60e6f20b619d1c0bb5fbb65b2402b4f220164
Philip Krag Nielsen <phil5520@edu.campusvejle.dk>
2023-08-04 21:32:16 +0200

    Add photo logic.

--- Application/Interfaces/IPhotoAccessor.cs
+++ Application/Interfaces/IPhotoAccessor.cs
@@ -5,7 +5,7 @@ namespace Application.Interfaces
 {
     public interface IPhotoAccessor
     {
-        Task<PhotoUploadResult> AddPhoto(IFormFile file);
-        Task<string> DeletePhoto(string publicId);
+        Task<PhotoUploadResult?> AddPhoto(IFormFile file);
+        Task<string?> DeletePhoto(string publicId);
     }
 }
