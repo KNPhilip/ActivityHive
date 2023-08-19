f3910c67ef6a6e26fb587032ed6dcc80a1fa2fdd
Philip Krag Nielsen <philipkragnielsen@gmail.com>
2023-08-19 18:53:44 +0200

    add filtering in the API

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -7,7 +7,7 @@ namespace API.Controllers
     public class ActivitiesController : ControllerTemplate
     {
         [HttpGet]
-        public async Task<IActionResult> GetActivities([FromQuery]PagingParams request)
+        public async Task<IActionResult> GetActivities([FromQuery]ActivityParams request)
         {
             return HandlePagedResult(await Mediator.Send(new List.Query{Params = request}));
         }
