0a91820b5a2ba29b1e83d3e837f0a77f41ede6bb
Philip Krag Nielsen <philipkragnielsen@gmail.com>
2023-08-20 21:19:00 +0200

    add profile events tab

---
+++ client-app/src/features/profile/ProfileActivities.tsx
@@ -0,0 +1,68 @@
+import { observer } from "mobx-react-lite";
+import { Card, Grid, Header, Tab, TabProps, Image } from "semantic-ui-react";
+import { useStore } from "../../app/stores/store";
+import { useEffect } from "react";
+import { SyntheticEvent } from "react-toastify/dist/utils";
+import { UserActivity } from "../../app/models/profile";
+import { Link } from "react-router-dom";
+import { format } from "date-fns";
+
+const panes = [
+    { menuItem: 'Future Events', pane: { key: 'future' } },
+    { menuItem: 'Past Events', pane: { key: 'past' } },
+    { menuItem: 'Hosting', pane: { key: 'hosting' } }
+];
+
+const ProfileActivities = () => {
+    const { profileStore } = useStore();
+    const { loadUserActivityes, profile, loadingActivities, userActivities } = profileStore;
+
+    useEffect(() => {
+        loadUserActivityes(profile!.username);
+    }, [loadUserActivityes, profile]);
+
+    const handleTabChange = (e: SyntheticEvent, data: TabProps) => {
+        loadUserActivityes(profile!.username, panes[data.activeIndex as number].pane.key)
+    }
+
+    return (
+        <Tab.Pane loading={loadingActivities}>
+            <Grid>
+                <Grid.Column width={16}>
+                    <Header floated="left" icon="calender" content={"Activities"} />
+                </Grid.Column>
+                <Grid.Column width={16}>
+                    <Tab 
+                        panes={panes}
+                        menu={{ secondary: true, pointing: true }}
+                        onTabChange={(e, data) => handleTabChange(e, data)}
+                    />
+                    <br />
+                    <Card.Group itemsPerRow={4}>
+                        {userActivities.map((activity: UserActivity) => (
+                            <Card
+                                as={Link}
+                                to={`/activities/${activity.id}`}
+                                key={activity.id}
+                            >
+                                <Image
+                                    src={`/assets/categoryImages/${activity.category}.jpg`}
+                                    style={{ minHeight: 100, objectFit: 'cover' }}
+                                />
+                                <Card.Content>
+                                    <Card.Header textAlign="center">{activity.title}</Card.Header>
+                                    <Card.Meta textAlign="center">
+                                        <div>{format(new Date(activity.date), 'do LLL')}</div>
+                                        <div>{format(new Date(activity.date), 'h:mm a')}</div>
+                                    </Card.Meta>
+                                </Card.Content>
+                            </Card>
+                        ))}
+                    </Card.Group>
+                </Grid.Column>
+            </Grid>
+        </Tab.Pane>
+    )
+}
+
+export default observer(ProfileActivities);
