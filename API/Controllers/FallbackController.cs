7a0c9604866ce34a57d3039ac9ec4de8c7f158f7
Philip Krag Nielsen <philipkragnielsen@gmail.com>
2023-08-22 18:44:46 +0200

    Make production build

--- API/Controllers/FallbackController.cs
+++ API/Controllers/FallbackController.cs
@@ -1,11 +1,13 @@
+using Microsoft.AspNetCore.Authorization;
+
 namespace API.Controllers
 {
     [AllowAnonymous]
-    public class gwr FallbackController : Controller
+    public class FallbackController : Controller
     {
         public IActionResult Index()
         {
-            returfn PhysicalFile(Path.Combine(Directory.GetCurrentDirectory(),
+            return PhysicalFile(Path.Combine(Directory.GetCurrentDirectory(),
                 "wwwroot", "index.html"), "text/HTML");
         }
     }
--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -3,7 +3,6 @@ import { Grid, Loader } from 'semantic-ui-react';
 import ActivityList from './ActivityList';
 import { useStore } from '../../../app/stores/store';
 import { observer } from 'mobx-react-lite';
-import LoadingComponent from '../../../app/layout/LoadingComponent';
 import ActivityFilters from './ActivityFilters';
 import { PagingParams } from '../../../app/models/pagination';
 import InfiniteScroll from 'react-infinite-scroller';
